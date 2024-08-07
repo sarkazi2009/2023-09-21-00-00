8853225bdf8c263ef6ac2b9b6a81bf285ac14c54
sarkazi2009 <sarkazi2009@gmail.com>
2024-08-07 17:22:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-
-example='Крокодил'
-print(example[0])
-print(example[-1])
-print(example[3:8])
-print(example[::-1])
-print(example[1:8:2])
+# This is a sample Python script.
+Course_name = 'Python'
+home_tasks = '12'
+All_hours = '1.5'
+Time_for_one_task = 1.5/12
+print('курс:'+Course_name+', всего задач:'+home_tasks+', затрачено часов:'+All_hours+', среднее время выполнения:'+str(Time_for_one_task))
